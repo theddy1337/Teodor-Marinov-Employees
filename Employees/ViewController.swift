a93bc5e963441c2df2a5dd0ae5ac0ebaba525ebb
Teodor Marinov <teodor.marinov@scalefocus.com>
2021-02-11 23:14:13 +0200

    Initial commit

---
+++ Employees/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Employees
+//
+//  Created by Teodor Marinov on 11.02.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
